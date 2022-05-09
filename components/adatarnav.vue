3f689f98b195a23c38aac71442fb5c4502caa5c5
osantonio <jlant1991@gmail.com>
2022-05-09 16:13:16 -0500

    staged

--- components/adatarnav.vue
+++ components/adatarnav.vue
@@ -4,16 +4,16 @@
             <div>
                 <img class="h-12" src="/adatar.png" alt="">
             </div>
-            <div class="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-              <a href="#" class="font-medium text-current hover:text-gray-900">Cargar Datos</a>
+            <div>
+              <a href="#" class="font-medium text-current hover:text-gray-900 pr-4">Cargar Datos</a>
 
-              <a href="#" class="font-medium text-current hover:text-gray-900">Universidad</a>
+              <a href="#" class="font-medium text-current hover:text-gray-900 pr-4">Universidad</a>
 
-              <a href="#" class="font-medium text-current hover:text-gray-900">Programa</a>
+              <a href="#" class="font-medium text-current hover:text-gray-900 pr-4">Programa</a>
 
-              <a href="#" class="font-medium text-current hover:text-gray-900">Acerca de</a>
+              <a href="#" class="font-medium text-current hover:text-gray-900 pr-4">Acerca de</a>
 
-              <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Iniciar</a>
+              <a href="#" class="font-medium text-indigo-600 hover:text-white pr-4">Iniciar</a>
             </div>
     </div>
 </template>
